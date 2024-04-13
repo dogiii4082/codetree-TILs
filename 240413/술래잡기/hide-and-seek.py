f9444903e94fcbe009eed255354683d70a91fd44
Wooseok Nam <93812867+wooseoknam@users.noreply.github.com>
2024-04-13 16:06:23 +0900

    Title: 술래잡기, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240413/술래잡기/hide-and-seek.py
+++ 240413/술래잡기/hide-and-seek.py
@@ -1,7 +1,6 @@
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-
 oppo = {
     0: 2,
     1: 3,
@@ -11,14 +10,14 @@ oppo = {
 
 
 def dist(x1, x2, y1, y2):
-    return abs(x1-x2) + abs(y1-y2)
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
 def move_hider(hiders):
     global hider_d
 
     for idx, [hx, hy] in enumerate(hiders):
-        if dist(sx, sy, hx, hy) > 3: continue
+        if dist(sx, hx, sy, hy) > 3: continue
 
         nx = hx + dx[hider_d[idx]]
         ny = hy + dy[hider_d[idx]]
@@ -41,27 +40,20 @@ def move_seeker(x, y):
     nx = x + dx[sd]
     ny = y + dy[sd]
 
-    temp_d = sd
-    if nx + 1 == ny and nx < center:        # to right
-        # sd = 1
-        temp_d = 1
+    if nx + 1 == ny and nx < center:  # to right
+        sd = 1
     elif nx + ny == n + 1 and nx < center:  # to down
-        # sd = 2
-        temp_d = 2
-    elif nx == ny and x > center:           # to left
-        # sd = 3
-        temp_d = 3
+        sd = 2
+    elif nx == ny and nx > center:  # to left
+        sd = 3
     elif nx + ny == n + 1 and nx > center:  # to up
-        # sd = 0
-        temp_d = 0
+        sd = 0
     elif nx == 1 and ny == 1:
-        # sd = 2
-        temp_d = 2
+        sd = 2
     elif nx == center and ny == center:
-        # sd = 0
-        temp_d = 0
+        sd = 0
 
-    return nx, ny, temp_d
+    return nx, ny
 
 
 def catch(x, y, d):
@@ -78,26 +70,26 @@ def catch(x, y, d):
         if [tx, ty] in trees: continue
 
         for idx, [hx, hy] in enumerate(hiders):
-           if [hx, hy] == [tx, ty]:
-               cnt += 1
-               hiders.pop(idx)
-               hider_d.pop(idx)
+            if [hx, hy] == [tx, ty]:
+                cnt += 1
+                hiders.pop(idx)
+                hider_d.pop(idx)
 
     return cnt
 
 
 if __name__ == "__main__":
-    n, m, h, k = map(int, input().split())      # m: 도망자, h: 나무, k: 반복
+    n, m, h, k = map(int, input().split())  # m: 도망자, h: 나무, k: 반복
 
-    sx, sy = n//2+1, n//2+1
-    center = n//2+1
+    sx, sy = n // 2 + 1, n // 2 + 1
+    center = n // 2 + 1
     sd = 0
 
     hiders = []
-    D = []          # [1, 2, 1]
-    hider_d = []     # [1, 2, 1]    # [0, 1, 2, 3] -> [상, 우, 하, 좌]
+    D = []  # [1, 2, 1]
+    hider_d = []  # [1, 2, 1]    # [0, 1, 2, 3] -> [상, 우, 하, 좌]
     for _ in range(m):
-        x, y, d = map(int, input().split())     # d -> 1: 좌우, 2: 상하
+        x, y, d = map(int, input().split())  # d -> 1: 좌우, 2: 상하
         hiders.append([x, y])
         D.append(d)
         hider_d.append(d)
@@ -108,11 +100,12 @@ if __name__ == "__main__":
         trees.append([x, y])
 
     ans = 0
-    for t in range(1, k+1):
+    for t in range(1, k + 1):
         hiders = move_hider(hiders)
 
-        sx, sy, sd = move_seeker(sx, sy)
-        
+        sx, sy = move_seeker(sx, sy)
+
         cnt = catch(sx, sy, sd)
         ans += t * cnt
+
     print(ans)
