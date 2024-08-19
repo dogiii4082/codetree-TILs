78b7efac6a5ddc5b7845b65094579b187b05a23a
Wooseok Nam <93812867+wooseoknam@users.noreply.github.com>
2024-08-19 19:29:32 +0900

    Title: 나무박멸, Time: 617ms, Memory: 29MB, Status: Passed - CodeTree

--- 240819/나무박멸/tree-kill-all.py
+++ 240819/나무박멸/tree-kill-all.py
@@ -1,85 +1,160 @@
+import copy
+
 dx = [-1, 1, 0, 0, -1, -1, 1, 1]
 dy = [0, 0, -1, 1, 1, -1, 1, -1]
+
 
 def is_tree(x, y):
     return board[x][y] > 0
 
+
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
+
 def count_adj_tree(x, y):
-    return sum(1 for i in range(4) if in_range(x + dx[i], y + dy[i]) and is_tree(x + dx[i], y + dy[i]))
+    ret = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if not in_range(nx, ny): continue
+        if is_tree(nx, ny): ret += 1
+    return ret
+
 
 def grow():
     for x in range(n):
         for y in range(n):
-            if is_tree(x, y):
-                board[x][y] += count_adj_tree(x, y)
+            if not is_tree(x, y): continue
+            cnt = count_adj_tree(x, y)
+            board[x][y] += cnt
 
-def spread():
-    tmp = [[0] * n for _ in range(n)]
-    for x in range(n):
-        for y in range(n):
-            if is_tree(x, y):
-                empty_cells = [(x + dx[i], y + dy[i]) for i in range(4) if in_range(x + dx[i], y + dy[i]) and board[x + dx[i]][y + dy[i]] == 0 and herbicide[x + dx[i]][y + dy[i]] == 0]
-                if empty_cells:
-                    spread_amount = board[x][y] // len(empty_cells)
-                    for nx, ny in empty_cells:
-                        tmp[nx][ny] += spread_amount
-    for x in range(n):
-        for y in range(n):
-            board[x][y] += tmp[x][y]
 
-def count_kill(x, y):
-    if not is_tree(x, y):
-        return 0
-    ret = board[x][y]
-    for i in range(4, 8):
-        for k in range(1, K + 1):
-            nx, ny = x + dx[i] * k, y + dy[i] * k
-            if not in_range(nx, ny) or board[nx][ny] <= 0:
-                break
-            ret += board[nx][ny]
+def count_adj_empty(x, y):
+    ret = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if not in_range(nx, ny): continue
+        if board[nx][ny] == 0: ret += 1
     return ret
 
-def kill(m):
+
+def spread():
+    tmp = copy.deepcopy(board)
+
+    for x in range(n):
+        for y in range(n):
+            if not is_tree(x, y): continue
+            e = count_adj_empty(x, y)
+            if e == 0: continue
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if not in_range(nx, ny): continue
+                if board[nx][ny] == 0:
+                    tmp[nx][ny] += board[x][y] // e
+
+    return tmp
+
+
+def count_kill(x, y):
+    ret = board[x][y]
+    if board[x][y] == 0: return 0
+    for i in range(4, 8):
+        for k in range(1, K + 1):
+            nx = x + dx[i] * k
+            ny = y + dy[i] * k
+
+            if not in_range(nx, ny): continue
+            # if board[nx][ny] == -1 or board[nx][ny] == 0: break
+            if board[nx][ny] <= 0: break
+
+            ret += board[nx][ny]
+
+    return ret
+
+
+def kill(m, c):
     global ans
-    max_kill, kx, ky = 0, -1, -1
+
+    tmp = copy.deepcopy(board)
+
+    cnt, kx, ky = -1, -1, -1
     for x in range(n):
         for y in range(n):
-            if is_tree(x, y):
-                kill_count = count_kill(x, y)
-                if kill_count > max_kill:
-                    max_kill, kx, ky = kill_count, x, y
+            if not is_tree(x, y): continue
 
-    if max_kill > 0:
-        ans += max_kill
-        herbicide[kx][ky] = c
-        board[kx][ky] = 0
-        for i in range(4, 8):
-            for k in range(1, K + 1):
-                nx, ny = kx + dx[i] * k, ky + dy[i] * k
-                if not in_range(nx, ny) or board[nx][ny] <= 0:
-                    break
-                herbicide[nx][ny] = c
-                board[nx][ny] = 0
+            # print(count_kill(x, y), x, y)
+            tmp[x][y] = count_kill(x, y)
 
-def update_herbicide():
+            if tmp[x][y] > cnt:
+                cnt = tmp[x][y]
+                kx = x
+                ky = y
+
+    if cnt > 0: ans += cnt
+
+    # print(kx, ky)
+    board[kx][ky] = - (m + c)
+    for i in range(4, 8):
+        for k in range(1, K+1):
+            nx = kx + dx[i]*k
+            ny = ky + dy[i]*k
+
+            if not in_range(nx, ny): continue
+            if board[nx][ny] <= 0 or is_wall[nx][ny]:
+                board[nx][ny] = - (m + c)
+                break
+
+            board[nx][ny] = - (m + c)
+
+
+def end_kill(m):    # m년 까지 제초제 존재
     for x in range(n):
         for y in range(n):
-            if herbicide[x][y] > 0:
-                herbicide[x][y] -= 1
+            if board[x][y] < 0 and -board[x][y] <= m:
+                if is_wall[x][y]: board[x][y] = -1
+                else: board[x][y] = 0
+
+
+def print_board():
+    for row in board:
+        print(*row)
+    print()
 
 if __name__ == "__main__":
     n, M, K, c = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(n)]
-    herbicide = [[0] * n for _ in range(n)]
+
+    is_wall = [[0 for _ in range(n)] for _ in range(n)]
+    for x in range(n):
+        for y in range(n):
+            if board[x][y] == -1:
+                is_wall[x][y] = 1
+
     ans = 0
 
-    for m in range(1, M + 1):
+    for m in range(1, M+1):
+        # print(f'--------{m}년-----------')
+        # print()
+
         grow()
-        spread()
-        kill(m)
-        update_herbicide()
+        # print("-------grow------")
+        # print_board()
+
+        board = spread()
+        # print("-------spread------")
+        # print_board()
+
+        kill(m, c)
+        # print("-------제초제 뿌리기------")
+        # print_board()
+
+        end_kill(m)
+        # print("-------제초제 제거------")
+        # print_board()
+
+        # print(f'ans: {ans}')
 
     print(ans)
