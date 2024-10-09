bb26f783871a861b536118a8c8edda0542916582
dogiiii <93812867+dogiii4082@users.noreply.github.com>
2024-10-09 16:04:42 +0900

    Title: 팩맨, Time: 487ms, Memory: 62MB, Status: Passed - CodeTree

--- 241009/팩맨/pacman.py
+++ 241009/팩맨/pacman.py
@@ -5,6 +5,7 @@ p_moves = []
 pdx = [-1, 0, 1, 0]
 pdy = [0, -1, 0, 1]
 
+
 def get_pacman_moves(cur):
     if len(cur) == 3:
         p_moves.append(cur[:])
@@ -15,10 +16,12 @@ def get_pacman_moves(cur):
         get_pacman_moves(cur)
         cur = cur[:-1]
 
+
 def clear_sub():
     for x in range(4):
         for y in range(4):
             sub_grid[x][y].clear()
+
 
 def try_duplicate():
     for x in range(4):
@@ -26,14 +29,17 @@ def try_duplicate():
             if not grid[x][y]: continue
             sub_grid[x][y].extend(grid[x][y])
 
+
 def in_range(x, y):
     return 0 <= x < 4 and 0 <= y < 4
+
 
 def can_go(x, y):
     if not in_range(x, y): return False
     if (x, y) == (px, py): return False
     if dead_grid[x][y]: return False
     return True
+
 
 def move_monster():
     nxt_grid = [[[] for _ in range(4)] for _ in range(4)]
@@ -49,17 +55,20 @@ def move_monster():
                 ny = y + dy[d]
                 can = True
                 while not can_go(nx, ny):
-                    d = max(1, (d+1)%9)
+                    d = max(1, (d + 1) % 9)
                     nx = x + dx[d]
                     ny = y + dy[d]
-                    if d == i: 
+                    if d == i:
                         can = False
                         break
-            
-                if can: nxt_grid[nx][ny].append(d)
-                else: nxt_grid[x][y].append(d)
+
+                if can:
+                    nxt_grid[nx][ny].append(d)
+                else:
+                    nxt_grid[x][y].append(d)
 
     return nxt_grid
+
 
 def pacman_move(t):
     global px, py
@@ -78,19 +87,38 @@ def pacman_move(t):
             x = x + pdx[int(d)]
             y = y + pdy[int(d)]
 
-            if not in_range(x, y): 
+            if not in_range(x, y):
                 is_out_range = True
                 break
 
             if not visited[x][y]:
                 cnt += len(grid[x][y])
                 visited[x][y] = True
-        
+
         if cnt > M and not is_out_range:
             M_dir = dir
             M = cnt
 
-    for d in M_dir: # 033
+    if M == 0:  # 먹을 게 없을 때
+        for dir in p_moves:
+            can = True
+            x = px
+            y = py
+            for d in dir:
+                x += pdx[int(d)]
+                y += pdy[int(d)]
+
+                if not in_range(x, y):
+                    can = False
+                    break
+            if can:
+                for d in dir:
+                    px += pdx[int(d)]
+                    py += pdy[int(d)]
+                    return
+
+
+    for d in M_dir:  # 033
         d = int(d)
         px += pdx[d]
         py += pdy[d]
@@ -98,11 +126,13 @@ def pacman_move(t):
             dead_grid[px][py].extend([t for _ in grid[px][py]])
             grid[px][py].clear()
 
+
 def dead_clear(t):
     for x in range(4):
         for y in range(4):
             if not dead_grid[x][y]: continue
-            dead_grid[x][y] = [i for i in dead_grid[x][y] if i+2 > t]
+            dead_grid[x][y] = [i for i in dead_grid[x][y] if i + 2 > t]
+
 
 def done_duplicate():
     for x in range(4):
@@ -110,6 +140,7 @@ def done_duplicate():
             if not sub_grid[x][y]: continue
             grid[x][y].extend(sub_grid[x][y])
             sub_grid[x][y].clear()
+
 
 if __name__ == "__main__":
     get_pacman_moves('')
@@ -120,13 +151,15 @@ if __name__ == "__main__":
 
     # Input
     m, T = map(int, input().split())
-    px, py = map(int, input().split()); px -= 1; py -= 1
+    px, py = map(int, input().split());
+    px -= 1;
+    py -= 1
     for _ in range(m):
         x, y, d = map(int, input().split())
-        grid[x-1][y-1].append(d)
+        grid[x - 1][y - 1].append(d)
 
     # Simulation
-    for t in range(1, T+1):
+    for t in range(1, T + 1):
         # print(f'====={t}=====')
         try_duplicate()
 
@@ -138,9 +171,6 @@ if __name__ == "__main__":
 
         done_duplicate()
 
-        # for row in grid:
-        #     print(*row)
-        # print()
         # for row in sub_grid:
         #     print(*row)
         # print()
